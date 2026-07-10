73fc823a2b4425b16cf249654b0cd354cf7f7294
Yannick Bosshard <yb@vthinvest.ch>
2026-07-10 11:40:28 +0200

    feat: connect contact form to Formspree

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -23,7 +23,7 @@ export default function ContactForm() {
     }
 
     try {
-      const res = await fetch('/api/contact', {
+      const res = await fetch('https://formspree.io/f/xqevrbao', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
